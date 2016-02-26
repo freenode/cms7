6c4422d5c5cd181752db8aba6ac49c27267bd279
Ed Kellett <e@kellett.im>
2016-02-26 05:05:35 +0000

    add yml inclusion support (!include path.yml)

--- cms7/config.py
+++ cms7/config.py
@@ -13,9 +13,8 @@ import yaml
 logger = logging.getLogger(__name__)
 
 def load(path):
-    with open(path, 'r') as f:
-        c = Config.load_from_file(f, Path(path).parent)
-        return c
+    c = Config.load_from_file(Path(path), Path(path).parent)
+    return c
 
 _MODULES = {
     'blog': Blog,
@@ -24,11 +23,33 @@ _MODULES = {
     'pages': Pages,
 }
 
+class IncludeLoader(yaml.Loader):
+    def __init__(self, stream, name):
+        self.__name = Path(name).parent
+        super().__init__(stream)
+
+
+    @classmethod
+    def load(cls, path):
+        with path.open('r') as f:
+            loader = IncludeLoader(f, path)
+            try:
+                return loader.get_single_data()
+            finally:
+                loader.dispose()
+
+    def include(self, node):
+        path = self.__name / self.construct_scalar(node)
+        return self.load(path)
+
+IncludeLoader.add_constructor('!include', IncludeLoader.include)
+
+
 class Config:
     @classmethod
     def load_from_file(cls, f, dir_):
         self = cls()
-        data = yaml.load(f)
+        data = IncludeLoader.load(f)
 
         self.name     = data['name']
         self.theme    = dir_ / data.get('theme', 'theme')
