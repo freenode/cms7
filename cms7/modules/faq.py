ade49a05f3120683ba682511ed0ab1eaebc4d27d
Ed Kellett <e@kellett.im>
2016-02-26 03:08:46 +0000

    add enough faq stuff to make it actually work

--- cms7/modules/faq.py
+++ cms7/modules/faq.py
@@ -17,8 +17,24 @@ class FaqEntry:
         self.source = source
 
     def render(self, gs):
-        self.content = self.source.render(gs)
-        return gs.render_template(self.template, title=self.title, article=self)
+        this = FaqEntryWrapper(gs, self)
+        return gs.render_template(self.template, entry=this)
+
+
+class FaqEntryWrapper:
+    def __init__(self, gs, entry):
+        self.gs = gs
+        self.entry = entry
+
+    def __getattr__(self, k):
+        return getattr(self.entry, k)
+
+    @property
+    def url(self):
+        return self.gs.url_for(self.name)
+
+    def render(self):
+        return self.source.render(self.gs)
 
 
 class FaqIndex:
@@ -28,13 +44,46 @@ class FaqIndex:
         self.title = data['title']
         self.cats = data['cats']
         self.template = data.get('template', 'faq_index.html')
-        self.promote = data.get('promote', [])
         self.entries = set()
         for cat in self.cats:
             self.entries |= self.faq.cats[cat]
+        self.promote = []
+        promote = data.get('promote', [])
+        for d in promote:
+            entry = self.faq.by_name[d['name']]
+            caption = d['caption'] or entry.title
+            icon = d['icon'] or None
+            self.promote.append({
+                'entry': entry,
+                'caption': caption,
+                'icon': icon
+            })
 
     def render(self, gs):
-        return gs.render_template('faq_index.html', title=self.title, index=self)
+        this = FaqIndexWrapper(gs, self)
+        return gs.render_template('faq_index.html', index=this)
+
+
+class FaqIndexWrapper:
+    def __init__(self, gs, index):
+        self.gs = gs
+        self.index = index
+        self.entries = sorted((FaqEntryWrapper(self.gs, x) for x in self.entries),
+                              key=lambda x: x.title)
+        self.promote = []
+        for item in self.index.promote:
+            self.promote.append({
+                'entry': FaqEntryWrapper(self.gs, item['entry']),
+                'caption': item['caption'],
+                'icon': item['icon']
+            })
+
+    def __getattr__(self, k):
+        return getattr(self.index, k)
+
+    @property
+    def url(self):
+        return self.gs.url_for(self.name)
 
 
 class Faq(Module):
@@ -46,6 +95,7 @@ class Faq(Module):
         self.index_root = PurePosixPath(index_root)
         self.indexes = indexes
 
+        self.by_name = {}
         self.cats = {}
 
     def prepare(self):
@@ -53,7 +103,9 @@ class Faq(Module):
             if d.is_dir():
                 for s in d.iterdir():
                     source = load_source(s)
-                    entry = FaqEntry(self, self.path_to_name(s), source)
+                    name = self.path_to_name(s)
+                    entry = FaqEntry(self, name, source)
+                    self.by_name[str(name)] = entry
                     self.cats.setdefault(d.name, set()).add(entry)
         self.log(logging.INFO, 'Found %d sources in %s (%d subdirs)',
                  sum(map(len, self.cats.values())), self.source.resolve(), len(self.cats))
@@ -64,6 +116,6 @@ class Faq(Module):
                 gen.add_render(entry.name, self.root / entry.slug, entry.render)
         for name, data in self.indexes.items():
             index = FaqIndex(self, name, data)
-            link = self.source / 'index' / index.name
+            link = self.path_to_name(self.source / 'index' / index.name)
             self.log(logging.INFO, 'Creating index %s with %d entries', link, len(index.entries))
             gen.add_render(link, self.index_root / index.name, index.render)
