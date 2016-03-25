03f4e3c4ad45cef859da98cc7e1766365230f023
Ed Kellett <e@kellett.im>
2016-03-25 08:47:35 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cms7',
-    version='0.1a3',
+    version='0.1a4',
     description='Simple static site generator',
     author='Ed Kellett',
     author_email='e@kellett.im',
