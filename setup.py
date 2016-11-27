c18c79ba17d35ca1b6089df6bbf20a67c7d9239f
Ed Kellett <e@kellett.im>
2016-11-27 20:45:40 +0000

    0.1a18

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cms7',
-    version='0.1a17',
+    version='0.1a18',
     description='Simple static site generator',
     author='Ed Kellett',
     author_email='e@kellett.im',
