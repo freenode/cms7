b73eba54bc86867582288665dc5e4ff21a60dccd
Svante <svante.b37@gmail.com>
2018-05-17 08:49:04 +0200

    Update setup.py for 0.1a20

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='cms7',
-    version='0.1a19',
+    version='0.1a20',
     description='Simple static site generator',
     author='Ed Kellett',
     author_email='e@kellett.im',
-    url='https://github.com/edk0/cms7',
+    url='https://github.com/freenode/cms7',
     packages=['cms7', 'cms7.modules'],
     install_requires=[
         'colorlog',
@@ -19,6 +19,12 @@ setup(
         'pathlib2',
         'python-dateutil',
         'pyyaml',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
     entry_points={
         'console_scripts': [
